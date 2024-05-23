73f661258b57d65d630a08426b7866df755f80bd
djw <329641926@qq.com>
2024-05-23 16:38:54 +0800

    main.cpp structure done!

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void sort(int arr[], int sz) {
+    
+}
+
+int main() {
+    int arr[] = {12, 65, 32, 90, 85, 9, 54};
+    int sz = sizeof(arr) / sizeof(arr[0]);
+
+    sort(arr, sz);
+
+    return 0;
+}
